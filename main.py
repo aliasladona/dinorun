631d4dfaf290485a5627276436063854c2977f84
SACAMEDELBOLSILLLO128919839871987 <mrubiocorzo@gmail.com>
2026-04-25 11:56:58 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+import ramdon
+import sys 
+import os 
+
+pygame.init()
+with,height= 900,400 
+screen=pygame.display.set_mode((with,height))
+pygame.display.set_gamma("diono run adventure")
+clock = pygame. time.Clock()
+font = pygame. font.SysFont("arial",24)
+
+#colors
+-
